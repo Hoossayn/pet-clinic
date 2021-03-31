7d8bf0c8cec8bc726c9cee7ad4d0ab09e83bc5d1
Hoossayn <abdulhakeemhusseni@gmail.com>
2021-03-31 16:25:58 +0100

    adding index page and controller

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService  extends CrudService<Owner, Long>{
+
+    Owner findByLastName(String lastname);
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/VetService.java
@@ -0,0 +1,9 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Vet;
+
+
+public interface VetService  extends CrudService<Vet, Long>{
+
+
+}
